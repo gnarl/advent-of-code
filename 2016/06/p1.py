6987201c3e1c2ed0aaeaec45cfbcabf7e8804b18
Chuck Fouts <fchuck@netapp.com>
2016-12-06 15:31:46 -0500

    Day 6

---
+++ 2016/06/p1.py
@@ -0,0 +1,22 @@
+import sys
+from collections import defaultdict
+
+def main():
+
+    
+    lines = [line.strip() for line in sys.stdin]
+    columns = [defaultdict(int) for x in range(0, len(lines[0]))]
+
+    for line in lines:
+        for i, c in enumerate(line):
+            columns[i][c] += 1
+                 
+    text = ''
+    for c in columns:
+        m = min(c, key=c.get)
+        text += m
+    print text
+
+
+if __name__ == '__main__':
+    main()
